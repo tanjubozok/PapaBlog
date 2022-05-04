fcb0725c913f4304a6f0ec085c3dd55c4d155cc8
Tanju BOZOK <tanjubozok@hotmail.com>
2022-05-04 18:21:02 +0400

    EntiyRepositoryler Shared klasörüne eklendi.

---
+++ PapaBlog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using PapaBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PapaBlog.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+
+        Task AddAsync(T entiny);
+        Task UpdateAsycn(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ PapaBlog.Shared/Data/Concrete/EfCore/EfEntityRepositoryBase.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PapaBlog.Shared.Data.Abstract;
+using PapaBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PapaBlog.Shared.Data.Concrete.EfCore
+{
+    public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+    {
+        private readonly DbContext _dbContext;
+
+        public EfEntityRepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(TEntity entiny)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entiny);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbContext.Set<TEntity>().AnyAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbContext.Set<TEntity>().CountAsync(predicate);
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            await Task.Run(() =>
+            {
+                _dbContext.Set<TEntity>().Remove(entity);
+            });
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (includeProperties.Any())
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return await query.SingleOrDefaultAsync();
+        }
+
+        public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbContext.Set<TEntity>();
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+
+            if (includeProperties.Any())
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task UpdateAsycn(TEntity entity)
+        {
+            await Task.Run(() =>
+            {
+                _dbContext.Set<TEntity>().Update(entity);
+            });
+        }
+    }
+}
