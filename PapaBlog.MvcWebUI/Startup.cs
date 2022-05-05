2046c17365a596e0a3dd64ca5d867fb478adf74c
Tanju BOZOK <tanjubozok@hotmail.com>
2022-05-05 20:47:48 +0400

    IServiceCollection classı eklendi.

--- PapaBlog.MvcWebUI/Startup.cs
+++ PapaBlog.MvcWebUI/Startup.cs
@@ -3,18 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PapaBlog.Services.Extensions;
 
 namespace PapaBlog.MvcWebUI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.LoadMyService();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ PapaBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PapaBlog.Data.Abstract;
+using PapaBlog.Data.Concrete;
+using PapaBlog.Data.Concrete.EfCore.Contexts;
+using PapaBlog.Services.Abstract;
+using PapaBlog.Services.Concrete;
+
+namespace PapaBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyService(this IServiceCollection services)
+        {
+            services.AddDbContext<PapaBlogContext>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IArticleService, ArticleManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+
+            return services;
+        }
+    }
+}
